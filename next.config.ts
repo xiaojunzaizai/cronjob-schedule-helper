dff592136a3eacf909b47cb87910f2a1984fa041
xiaojunzaizai <xiaojunzaizai@gmail.com>
2026-05-15 13:34:04 -0400

    update github action setup

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
 import type { NextConfig } from "next";
 
+const repo = "cronjob-schedule-helper";
+const isGithubPages = process.env.GITHUB_PAGES === "true";
+
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "standalone",
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: isGithubPages ? `/${repo}` : "",
+  assetPrefix: isGithubPages ? `/${repo}/` : "",
 };
 
 export default nextConfig;
